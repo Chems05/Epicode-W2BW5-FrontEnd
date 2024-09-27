28b664471e4c4104f5ac3b65cc144d273ad0b8ec
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-09-27 15:02:44 +0200

    add fetch for list user

--- EpicodeBuildWeek5/src/components/UserList.jsx
+++ EpicodeBuildWeek5/src/components/UserList.jsx
@@ -1,33 +1,48 @@
 import { useEffect, useState } from "react";
-import { getUsers, deleteUser } from "../api"; // Dovresti avere queste funzioni nell'API
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const data = await getUsers();
-        setUsers(data);
-      } catch (error) {
-        console.error("Error fetching users:", error);
-      }
-    };
-
-    fetchUsers();
-  }, []);
-
-  const handleDelete = async (id) => {
-    try {
-      await deleteUser(id); // Chiamata API per eliminare l'utente
-      // Filtra l'utente eliminato dall'array
-      setUsers((prevUsers) => prevUsers.filter((user) => user.id !== id));
-    } catch (error) {
-      console.error("Error deleting user:", error);
-    }
+  const fetchUserList = () => {
+    console.log("Fetching invoices...");
+    fetch(`http://localhost:3001/users`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((response) => {
+        console.log("Response received:", response);
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Errore nel recupero delle fatture");
+        }
+      })
+      .then((result) => {
+        console.log("Fatture fetched:", result.data);
+        setUsers(result.data);
+      })
+      .catch((error) => console.log("Fetch error:", error));
   };
 
-  return <div></div>;
+  useEffect(() => {
+    fetchUserList();
+  }, []);
+
+  return (
+    <div>
+      <h2>Fatture</h2>
+      <ul>
+        {users.map((invoice) => (
+          <li key={invoice.id}>
+            {invoice.numero} - {invoice.importo} - {invoice.data} -{" "}
+            {invoice.stato}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default UserList;
